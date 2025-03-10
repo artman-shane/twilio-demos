c16edc214dfef99d120b226cf2260289de7ac2da
Shane <mrshaneartman@gmail.com>
2025-03-10 10:16:25 -0400

    Leveraging material ui sx props instead of css where possible

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,17 @@ import Footer from "../components/Footer";
 export default function Layout({ children }) {
   return (
     <html lang="en">
+      <link
+        href="https://fonts.cdnfonts.com/css/twilio-sans-mono"
+        rel="stylesheet"
+      ></link>
       <body>
         <ThemeProvider theme={theme}>
           <ErrorBoundary>
             <div id="root">
               <Header />
               <div className="content">{children}</div>
-              <Footer className="footer" />
+              <Footer className="footer"/>
             </div>
           </ErrorBoundary>
         </ThemeProvider>
